3d73fb4773a58b839abf724b1318c5c5db17550a
mateus.almeida <mateuszilio21@gmail.com>
2026-01-18 20:59:20 -0300

    ajuste app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 /* 🔥 corrige o erro no navegador de 
 Response to preflight request doesn't pass access control check
-No 'Access-Control-Allow-Origin' header is present*/
+No 'Access-Control-Allow-Origin' header is present
 app.options('*', cors({
   origin: [
     'https://api-financas-frontend.onrender.com',
@@ -30,6 +30,7 @@ app.options('*', cors({
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+*/
 
 app.use(express.json());
 
