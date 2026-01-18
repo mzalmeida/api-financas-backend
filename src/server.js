41cd877692b02f0b727dbc2adf64269f2c45b371
mateus.almeida <mateuszilio21@gmail.com>
2026-01-18 20:30:22 -0300

    ajute server.js

--- src/server.js
+++ src/server.js
@@ -1,38 +1,8 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 
-const authRoutes = require("./routes/auth");
-const gastosRoutes = require("./routes/gastos");
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-/**
- * 🔓 CORS ABERTO PARA DEMO PÚBLICA
- * (necessário para frontend no Render)
- */
-app.use(cors());
-
-app.use(express.json());
-
-// rota teste
-app.get("/", (req, res) => {
-  res.json({ status: "API rodando" });
-});
-
-// rota healthcheck
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    service: "api-financas",
-    timestamp: new Date().toISOString()
-  });
-});
-
-// rotas
-app.use("/auth", authRoutes);
-app.use(gastosRoutes);
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Servidor rodando na porta", process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
