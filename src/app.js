a4c5a59d15ec5519d2a8feb4f0ef050166125309
mateus.almeida <mateuszilio21@gmail.com>
2026-08-12 22:36:34 -0300

    Allow movement category updates through CORS

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
     }
     return callback(new Error("Origin not allowed by CORS"));
   },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
